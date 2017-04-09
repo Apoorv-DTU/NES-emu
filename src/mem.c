841a7f5ff7b6c51d7cc981c24b77924d2899ec8a
Apoorv Goel <Apoorv Goel>
2017-04-09 16:54:10 +0530

    Better implementation of LD and ST cmds

--- src/mem.c
+++ src/mem.c
@@ -1,137 +1,103 @@
 #include "mem.h"
-#include "registers.h"
 
-static uint8_t mem[0x10000] = {0};
-
-uint8_t m_read(uint16_t loc, Mode m, reg_t* regs) {
+uint16_t resolv_indirect(uint8_t addr, state_t* state, uint8_t x, uint8_t y) {
+    uint16_t low_addr = (addr + x) % 0xff; 
     
-    uint8_t value;
+    uint8_t low = state->mem[low_addr];
+    uint8_t high = state->mem[low_addr+1];
+    
+    uint8_t norm_addr = (high << 8) + low;
 
-    switch(m) {
-        case ACC:
-            value = regs->A;
-        break;
-
-        case IMM:
-            value = loc;
-        break;
-
-        case ZER:
-            value = mem[loc];
-        break;
-
-        case ZEX:
-            // addition wraps if result > 0xff
-            value = mem[(loc+(regs->X)) % 0xff];
-        break;
-
-        case ZEY:
-            // addition wraps if result > 0xff
-            value = mem[(loc+(regs->Y)) % 0xff];
-        break;
-
-        case REL:
-            value = mem[loc+(regs->PC)];
-        break;
-        
-        case ABS:
-            value = mem[loc];
-        break;
-
-        case ABX:
-            value = mem[loc+(regs->X)];
-        break;
-
-        case ABY:
-            value = mem[loc+(regs->Y)];
-        break;
-
-        case IND:
-            // little endian
-            value = mem[(mem[loc+1] << 8) + mem[loc]];
-        break;
-
-        case IDX:
-            value = m_read((loc+(regs->X)) % 0xff, IND, regs);
-        break;
-
-        case IDY:
-            value = mem[(mem[loc+1] << 8) + mem[loc]+(regs->Y)];
-        break;
-
-        default:
-            //Invalid Addressing Mode
-            value = 0;
-        break;
-    }
-
-    return value;
+    return norm_addr + y;
 }
 
-void m_write(uint16_t loc, Mode m, uint8_t value, const reg_t regs) {
-
-    switch(m) {
-        case ZER:
-            mem[loc] = value;
-        break;
-
-        case ZEX:
-            mem[(loc + (regs.X)) % 0xff] = value;
-        break;
-
-        case ZEY:
-            mem[(loc + (regs.Y)) % 0xff] = value;
-        break;
-
-        case ABS:
-            mem[loc] = value;
-        break;
-
-        case ABX:
-            mem[loc + (regs.X)] = value;
-        break;
-
-        case ABY:
-            mem[loc + (regs.Y)] = value;
-        break;
-
-        case IDX:
-            mem[(mem[loc + (regs.X) + 1] << 8) + mem[loc + (regs.X)]] = value;
-        break;
-
-        case IDY:
-            mem[(mem[loc+1] << 8) + mem[loc] + (regs.Y)] = value;
-        break;
-
-        default:
-            // invalid Mode for writing to memory
-        break;
-    }
+uint16_t resolv_abs(uint8_t addr_l, uint8_t addr_h, uint8_t offset) {
+    return addr_l + (addr_h << 8) + offset;
 }
 
-void LDA(uint16_t loc, Mode m, reg_t* regs) {
-    regs->A = m_read(loc, m, regs);
-    set_PC_NZ(regs, regs->A);
+uint16_t resolv_zer(uint8_t addr, uint8_t offset) {
+    return (uint16_t)((addr + offset) % 0xff);
 }
 
-void LDX(uint16_t loc, Mode m, reg_t* regs) {
-    regs->X = m_read(loc, m, regs);
-    set_PC_NZ(regs, regs->X);
+#define LD_AB(__Reg, __Off) uint16_t addr = resolv_abs(G_LOW8(args), G_HIGH8(args), __Off); \
+                            state->__Reg = state->mem[addr];                                 \
+                                                                                            \
+                            if((addr % 0x0fff) == (state->PC % 0x0fff))                     \
+                                return SUCCESS;                                             \
+                            else return CROSS_PAGE;             
+
+#define LD_ZE(__Reg, __Off) uint16_t addr = resolv_zer(G_LOW8(args), __Off);  \
+                            state->__Reg = state->mem[addr];                 \
+                            return SUCCESS;
+
+#define LD_IN(__Reg, __OffX, __OffY) uint16_t addr = resolv_indirect(G_LOW8(args), state , __OffX, __OffY); \
+                                     state->__Reg = state->mem[addr];                                             \
+                                                                                                           \
+                                     if((addr % 0x0fff) == (state->PC % 0x0fff))                           \
+                                         return SUCCESS;                                                   \
+                                     else return CROSS_PAGE;
+
+#define ST_AB(__Reg, __Off) uint16_t addr = resolv_abs(G_LOW8(args), G_HIGH8(args), __Off); \
+                            state->mem[addr] = state->__Reg;                                        \
+                            return SUCCESS;
+
+#define ST_ZE(__Reg, __Off) uint16_t addr = resolv_ze(G_LOW8(args), __Off); \
+                            state->mem[addr] = state->__Reg;                       \
+                            return SUCCESS;
+
+Status LDA_IMM(arg_t args, state_t* state) {
+    state->A = G_LOW8(args);
+    return SUCCESS;
+}
+Status LDA_ABS(arg_t args, state_t* state) { LD_AB(A, 0) }
+Status LDA_ABX(arg_t args, state_t* state) { LD_AB(A, state->X) }
+Status LDA_ABY(arg_t args, state_t* state) { LD_AB(A, state->Y) }
+Status LDA_ZER(arg_t args, state_t* state) { LD_ZE(A, 0) }
+Status LDA_ZEX(arg_t args, state_t* state) { LD_ZE(A, state->X) }
+Status LDA_ZEY(arg_t args, state_t* state) { LD_ZE(A, state->Y) }
+Status LDA_IND(arg_t args, state_t* state) { LD_IN(A, 0, 0) }
+Status LDA_IDX(arg_t args, state_t* state) { LD_IN(A, state->X, 0) }
+Status LDA_IDY(arg_t args, state_t* state) { LD_IN(A, 0, state->Y) }
+
+Status LDX_IMM(arg_t args, state_t* state) {
+    state->X = G_LOW8(args);
+    return SUCCESS;
+}
+Status LDX_ABS(arg_t args, state_t* state) { LD_AB(X, 0) }
+Status LDX_ABY(arg_t args, state_t* state) { LD_AB(X, state->Y) }
+Status LDX_ZER(arg_t args, state_t* state) { LD_ZE(X, 0) }
+Status LDX_ZEY(arg_t args, state_t* state) { LD_ZE(X, state->Y) }
+
+Status LDY_IMM(arg_t args, state_t* state) {
+    state->Y = G_LOW8(args);
+    return SUCCESS;
+}
+Status LDY_ABS(arg_t args, state_t* state) { LD_AB(Y, 0) }
+Status LDY_ABX(arg_t args, state_t* state) { LD_AB(Y, state->X) }
+Status LDY_ZER(arg_t args, state_t* state) { LD_ZE(Y, 0) }
+Status LDY_ZEX(arg_t args, state_t* state) { LD_ZE(Y, state->X) }
+
+
+Status STA_ABS(arg_t args, state_t* state) { ST_AB(A, 0) }
+Status STA_ABX(arg_t args, state_t* state) { ST_AB(A, state->X) }
+Status STA_ABY(arg_t args, state_t* state) { ST_AB(A, state->Y) }
+Status STA_ZER(arg_t args, state_t* state) { ST_ZE(A, 0) }
+Status STA_ZEX(arg_t args, state_t* state) { ST_ZE(A, state->X) }
+Status STA_IDX(arg_t args, state_t* state) {
+    uint16_t addr = resolv_indirect(G_LOW8(args), G_HIGH8(args), state->X, 0);
+    state->mem[addr] = state->A;
+    return SUCCESS;
+}
+Status STA_IDY(arg_t args, state_t* state) {
+    uint16_t addr = resolv_indirect(G_LOW8(args), G_HIGH8(args), 0, state->Y);
+    state->mem[addr] = state->A;
+    return SUCCESS;
 }
 
-void LDY(uint16_t loc, Mode m, reg_t* regs) {
-    regs->Y = m_read(loc, m, regs);
-    set_PC_NZ(regs, regs->Y);
-}
+Status STX_ABS(arg_t args, state_t* state) { ST_AB(X, 0) }
+Status STX_ZER(arg_t args, state_t* state) { ST_ZE(X, 0) }
+Status STX_ZEY(arg_t args, state_t* state) { ST_ZE(X, state->Y) }
 
-void STA(uint16_t loc, Mode m, const reg_t regs) {
-    m_write(loc, m, regs.A, regs);
-}
-
-void STX(uint16_t loc, Mode m, const reg_t regs) {
-    m_write(loc, m, regs.X, regs);
-}
-
-void STY(uint16_t loc, Mode m, const reg_t regs) {
-    m_write(loc, m, regs.Y, regs);
-}
+Status STY_ABS(arg_t args, state_t* state) { ST_AB(Y, 0) }
+Status STY_ZER(arg_t args, state_t* state) { ST_ZE(Y, 0) }
+Status STY_ZEX(arg_t args, state_t* state) { ST_ZE(Y, state->X) }
--- src/mem.h
+++ src/mem.h
@@ -2,6 +2,7 @@
 #define MEM_H
 
 #include "registers.h"
+#include "cmd.h"
 
 enum addr_mode { ACC, IMM, ZER, ZEX, ZEY, REL, 
             ABS, ABX, ABY, IND, IDX, IDY 
@@ -9,17 +10,63 @@ enum addr_mode { ACC, IMM, ZER, ZEX, ZEY, REL,
 
 typedef enum addr_mode Mode;
 
-uint8_t m_read(uint16_t loc, Mode m, reg_t* regs);
-void m_write(uint16_t loc, Mode m, uint8_t value, const reg_t regs);
+uint16_t resolv_indirect(uint8_t addr, state_t* state, uint8_t x, uint8_t y);
 
-// Load commands
-void LDA(uint16_t loc, Mode m, reg_t* regs);
-void LDX(uint16_t loc, Mode m, reg_t* regs);
-void LDY(uint16_t loc, Mode m, reg_t* regs);
+uint16_t resolv_abs(uint8_t addr_l, uint8_t addr_h, uint8_t offset);
+uint16_t resolv_zer(uint8_t addr_l, uint8_t offset);
 
-// Store commands
-void STA(uint16_t loc, Mode m, const reg_t regs);
-void STX(uint16_t loc, Mode m, const reg_t regs);
-void STY(uint16_t loc, Mode m, const reg_t regs);
+// LDA commands
+Status LDA_IMM(arg_t args, state_t* state);
+
+Status LDA_ABS(arg_t args, state_t* state);
+Status LDA_ABX(arg_t args, state_t* state);
+Status LDA_ABY(arg_t args, state_t* state);
+
+Status LDA_ZER(arg_t args, state_t* state);
+Status LDA_ZEX(arg_t args, state_t* state);
+Status LDA_ZEY(arg_t args, state_t* state);
+
+Status LDA_IND(arg_t args, state_t* state);
+Status LDA_IDY(arg_t args, state_t* state);
+Status LDA_IDX(arg_t args, state_t* state);
+
+// LDX commands
+Status LDX_IMM(arg_t args, state_t* state);
+
+Status LDX_ZER(arg_t args, state_t* state);
+Status LDX_ZEY(arg_t args, state_t* state);
+
+Status LDX_ABS(arg_t args, state_t* state);
+Status LDX_ABY(arg_t args, state_t* state);
+
+// LDY commands
+Status LDY_IMM(arg_t args, state_t* state);
+
+Status LDY_ZER(arg_t args, state_t* state);
+Status LDY_ZEX(arg_t args, state_t* state);
+
+Status LDY_ABS(arg_t args, state_t* state);
+Status LDY_ABX(arg_t args, state_t* state);
+
+// STA commands
+Status STA_ZER(arg_t args, state_t* state);
+Status STA_ZEX(arg_t args, state_t* state);
+
+Status STA_ABS(arg_t args, state_t* state);
+Status STA_ABX(arg_t args, state_t* state);
+Status STA_ABY(arg_t args, state_t* state);
+
+Status STA_IDX(arg_t args, state_t* state);
+Status STA_IDY(arg_t args, state_t* state);
+
+// STX commands
+Status STX_ABS(arg_t args, state_t* state);
+Status STX_ZEY(arg_t args, state_t* state);
+Status STX_ZER(arg_t args, state_t* state);
+
+// STY commands
+Status STY_ABS(arg_t args, state_t* state);
+Status STY_ZER(arg_t args, state_t* state);
+Status STY_ZEX(arg_t args, state_t* state);
 
 #endif
